e0de1ffc56c56f08bee98023df2b89eabe14462a
Simon Yan <simon.xiaogang.yan@gmail.com>
2024-11-22 08:33:29 +1300

    update form validation test

--- src/tests/app.spec.tsx
+++ src/tests/app.spec.tsx
@@ -10,6 +10,40 @@ describe("App test", () => {
     // Check if the Register card form is rendered by default
     expect(screen.getByText("Register card form")).toBeInTheDocument();
     expect(screen.queryByText("Menu")).not.toBeInTheDocument();
+    
+    // Check form fileds are rendered
+    expect(screen.getByLabelText("Credit card number")).toBeInTheDocument();
+    expect(screen.getByLabelText("CVC")).toBeInTheDocument();
+    expect(screen.getByLabelText("Expiry")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
+
+    // Check the Submit button is disabled initially
+    expect(screen.getByRole("button", { name: "Submit" })).toBeDisabled();
+
+
+    // Fill the form successfully
+    const cardNumberInput = screen.getByLabelText("Credit card number");
+    const cvcInput = screen.getByLabelText("CVC");
+    const expiryDateInput = screen.getByLabelText("Expiry");
+    const submitButton = screen.getByRole("button", { name: "Submit" });
+
+    // Check the form is not valid if the card number is invalid
+    fireEvent.change(cardNumberInput, { target: { value: "12345678" } });
+    expect(submitButton).toBeDisabled();
+
+    // Check the form is not valid if the cvc is invalid
+    fireEvent.change(cardNumberInput, { target: { value: "1234567812345678" } });
+    fireEvent.change(cvcInput, { target: { value: "12" } });
+    expect(submitButton).toBeDisabled();
+
+    // Check the form is not valid if the expiry date is invalid
+    fireEvent.change(cvcInput, { target: { value: "123" } });
+    fireEvent.change(expiryDateInput, { target: { value: "1" } });
+    expect(submitButton).toBeDisabled();
+
+    // Check the form is valid if all fields are filled correctly
+    fireEvent.change(expiryDateInput, { target: { value: "12/25" } });
+    expect(submitButton).toBeEnabled();
 
     // Click on the Menu button
     fireEvent.click(screen.getByLabelText("Open Menu"));
