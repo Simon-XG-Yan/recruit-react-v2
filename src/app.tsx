54c34389ffa3ad2c20e351025cac59b7b8b6479c
Simon Yan <simon.xiaogang.yan@gmail.com>
2024-11-22 06:50:03 +1300

    set up project structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import * as styles from "./app.module.scss";
+import * as styles from "./styles/app.module.scss";
 
 export const App = () => {
   return <h1 className={styles.demo}>Welcome to your technical test!</h1>;
