ac54838ce44b8774e6d1cd866319fd407a94cc45
Simon Yan <simon.xiaogang.yan@gmail.com>
2024-11-22 07:30:54 +1300

    add styling

--- src/components/RegisterCardForm/RegisterCardForm.tsx
+++ src/components/RegisterCardForm/RegisterCardForm.tsx
@@ -4,31 +4,31 @@ import * as styles from "./RegisterCardForm.module.scss";
 
 export const RegisterCardForm: React.FC = () => {
     return (
-        <div>        
+        <div className={styles.registerCardForm}>        
             <h2>Welcome, User</h2>
 
-            <form>
-                <label htmlFor="cardNumber">
+            <form className={styles.form}>
+                <label htmlFor="cardNumber" className={styles.label}>
                     Credit card number
                 </label>
-                <input type="text" id="cardNumber" name="cardNumber" />
+                <input type="text" id="cardNumber" name="cardNumber" className={styles.input} placeholder="1234 5678 9012 3456" />
 
-                <div>
+                <div className={styles.cvcExpiryDate}>
                     <div>
-                        <label htmlFor="cvc">
+                        <label htmlFor="cvc" className={styles.label}>
                             CVC
                         </label>
-                        <input type="text" id="cvc" name="cvc" />
+                        <input type="text" id="cvc" name="cvc" className={styles.input} placeholder="123" />
                     </div>
                     <div>
-                        <label htmlFor="expiryDate">
+                        <label htmlFor="expiryDate" className={styles.label}>
                             Expiry
                         </label>
-                        <input type="text" id="expiryDate" name="expiryDate" />
+                        <input type="text" id="expiryDate" name="expiryDate" className={styles.input} placeholder="MM/YY" />
                     </div>
                 </div>
 
-                <button type="submit">
+                <button type="submit" className={styles.submitButton}>
                     Submit
                 </button>
             </form>
